7683c757e90742f65753af17ff96220fa0d6064e
yanbo0039 <yanbo0039@yeah.net>
2019-09-26 14:07:55 +0800

    8.使用applicationContext初始化bean时启动懒加载 14:06

--- src/test/TestBeanFactoryAndApplicationContext.java
+++ src/test/TestBeanFactoryAndApplicationContext.java
@@ -1,8 +1,7 @@
 package test;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class TestBeanFactoryAndApplicationContext {
 
@@ -11,10 +10,29 @@ public class TestBeanFactoryAndApplicationContext {
 		/**
 		 * 1. BeanFactory
 		 */
-		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+//		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+//		
+//		A a = (A)factory.getBean("a");
+//		
+//		a.sayHello("sunyanchun");
 		
-		A a = (A)factory.getBean("a");
+		//2.applicationContext
+		//①文件放在硬盘目录下，怎么读取？
 		
-		a.sayHello("sunyanchun");
+		//ApplicationContext act=new FileSystemXmlApplicationContext("file:d:\\applicationContext.xml");
+		//②文件放在项目目录下
+		//ApplicationContext act=new FileSystemXmlApplicationContext("file:config\\applicationContext.xml");
+		//③文件放在类路径下
+		//ApplicationContext act=new FileSystemXmlApplicationContext("classpath:applicationContext.xml");
+		//④很多个beans.xml文件如何读取
+		ApplicationContext act = new FileSystemXmlApplicationContext(
+				new String[]{"classpath:applicationContext.xml","classpath:beans-order.xml"});
+		
+		C c = (C) act.getBean("cc");
+		c.print("haogen");
+		//A a=act.getBean(test.A.class);
+		
+		
+		//a.sayHello("likaiqing");
 	}
 }
